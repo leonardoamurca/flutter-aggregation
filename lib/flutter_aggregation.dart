16ee90463d8624966aea05bcbe51b6e513e8d52b
Matheus Marques <marquesm91@gmail.com>
2019-06-01 13:19:11 -0300

    feat: finish translate method

--- lib/flutter_aggregation.dart
+++ lib/flutter_aggregation.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_aggregation/pages/home.dart';
+import 'services/translate.dart';
 
 class FlutterAggregation extends StatelessWidget {
+  final BaseTranslate translate = new Translate();
+
   final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
     "Dashboard": (context) => Home()
   };
 
   @override
   Widget build(BuildContext context) {
+    translate
+        .translate(targetLanguage: 'fr', text: 'eu amo flutter')
+        .then((translation) => print(translation.translatedText));
+
     return MaterialApp(
         title: 'Flutter Aggregation',
         theme: ThemeData(
