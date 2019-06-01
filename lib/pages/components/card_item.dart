31e029f53390f04d8bcc761820e6a00b20e776d9
Leonardo Murca <leonardoamurca@gmail.com>
2019-06-01 15:35:35 -0300

    Adding the logic to get the assets
    Co-authored-by: Cloves Moreira Junior <xjunior20@gmail.com>

--- lib/pages/components/card_item.dart
+++ lib/pages/components/card_item.dart
@@ -2,21 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_aggregation/pages/home.dart';
 
-class CardItem extends StatelessWidget {
+class CardItem extends StatefulWidget {
   CardItem({this.cardModel});
-  final CardModel cardModel;
+  final FeedItem cardModel;
+
+  @override
+  State<StatefulWidget> createState() => _CardItem(cardModel: cardModel);
+}
+
+class _CardItem extends State<CardItem> {
+  _CardItem({this.cardModel});
+  final FeedItem cardModel;
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        ListTile(
-          leading: const Icon(Icons.ac_unit),
-          title: Text(cardModel.title),
-          subtitle: Text('Ola mundo'),
-        ),
-      ],
-    ));
+    return Hero(
+      tag: 'cardItem',
+      child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8.0),
+            ),
+          ),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: Stack(children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1,
+              child: Image.network(cardModel.image),
+            ),
+            Material(
+              child: ListTile(
+                title: Text(cardModel.title),
+                subtitle: Text(cardModel.link),
+              ),
+            ),
+          ]
+        )),
+    );
   }
 }
