574f8eba642c9d9c06eaf534420feadbaf1d814a
Leonardo Murca <leonardoamurca@gmail.com>
2019-06-01 12:39:52 -0300

    Displaying CardModels

---
+++ lib/pages/components/card_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_aggregation/pages/home.dart';
+
+class CardItem extends StatelessWidget {
+  CardItem({this.cardModel});
+  final CardModel cardModel;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ListTile(
+            leading: const Icon(Icons.ac_unit), title: Text(cardModel.title))
+      ],
+    ));
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aggregation/pages/components/card_item.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var items = getMessages();
     return Scaffold(
       appBar: AppBar(
         title: Text('Welcome to Flutter'),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, item) {
+            return CardItem(
+              cardModel: items[item],
+            );
+          },
+        ),
       ),
     );
   }
+
+  List<CardModel> getMessages() {
+    return [
+      CardModel(
+          title: 'Flutter title',
+          imageUrl: 'Flutter ImageUrl',
+          message: 'Flutter message'),
+      CardModel(
+          title: 'Flutter title',
+          imageUrl: 'Flutter ImageUrl',
+          message: 'Flutter message'),
+      CardModel(
+          title: 'Flutter title',
+          imageUrl: 'Flutter ImageUrl',
+          message: 'Flutter message'),
+      CardModel(
+          title: 'Flutter title',
+          imageUrl: 'Flutter ImageUrl',
+          message: 'Flutter message'),
+    ];
+  }
+}
+
+class CardModel {
+  CardModel({this.title, this.imageUrl, this.message});
+  final String title;
+  final String imageUrl;
+  final String message;
 }
