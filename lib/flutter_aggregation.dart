906b0265ef4df40e66788517aaacecbb561b83a5
Matheus Marques <marquesm91@gmail.com>
2019-06-01 15:26:51 -0300

    feat: add feed service

--- lib/flutter_aggregation.dart
+++ lib/flutter_aggregation.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_aggregation/pages/home.dart';
-import 'services/translate.dart';
+import 'services/feed.dart';
 
 class FlutterAggregation extends StatelessWidget {
-  final BaseTranslate translate = new Translate();
+  final BaseFeedResponse feedResponse = new FeedResponse();
 
   final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
     "Dashboard": (context) => Home()
@@ -12,9 +12,34 @@ class FlutterAggregation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    translate
-        .translate(targetLanguage: 'fr', text: 'eu amo flutter')
-        .then((translation) => print(translation.translatedText));
+    // feedResponse
+    //     .get(
+    //   feedUrl: "https://medium.com/feed/flutter",
+    //   targetLanguage: 'pt-BR',
+    // )
+    //     .then((feed) {
+    //   print('feed.title: ${feed.title}');
+    //   for (var feedItem in feed.items) {
+    //     print('feedItem.title: ${feedItem.title}');
+    //     print('feedItem.image: ${feedItem.image}');
+    //     print('feedItem.link: ${feedItem.link}');
+    //     print('feedItem.translatedLink: ${feedItem.translatedLink}');
+    //   }
+    // });
+
+    feedResponse
+        .getAll(
+      targetLanguage: 'pt-BR',
+    )
+        .then((feed) {
+      print('feed.title: ${feed.title}');
+      for (var feedItem in feed.items) {
+        print('feedItem.title: ${feedItem.title}');
+        print('feedItem.image: ${feedItem.image}');
+        print('feedItem.link: ${feedItem.link}');
+        print('feedItem.translatedLink: ${feedItem.translatedLink}');
+      }
+    });
 
     return MaterialApp(
         title: 'Flutter Aggregation',
